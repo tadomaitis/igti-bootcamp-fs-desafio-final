01c176c1dc34c84fe0126b79159741404eb168df
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-19 10:36:46 -0300

    Make all transaction fields required

--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -1,15 +1,42 @@
 import mongoose from "mongoose";
 
 let transactionSchema = mongoose.Schema({
-  description: String,
-  value: Number,
-  category: String,
-  year: Number,
-  month: Number,
-  day: Number,
-  yearMonth: String,
-  yearMonthDay: String,
-  type: String,
+  description: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  month: {
+    type: Number,
+    required: true,
+  },
+  day: {
+    type: Number,
+    required: true,
+  },
+  yearMonth: {
+    type: String,
+    required: true,
+  },
+  yearMonthDay: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
 });
 
 const transactionModel = mongoose.model("transaction", transactionSchema);
